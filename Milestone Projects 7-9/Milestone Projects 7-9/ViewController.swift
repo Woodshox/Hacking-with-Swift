4c3979ad13546b39d258988351f422812d509dcc
Woodshox <1aleksandrpavlov@gmail.com>
2022-07-05 12:27:05 +0200

    Init Milsestone Project 7-9

---
+++ Milestone Projects 7-9/Milestone Projects 7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Projects 7-9
+//
+//  Created by Aleksandr Pavlov on 05.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
